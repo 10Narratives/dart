1497796157968c0a244252f6c1b491cfbf13f19c
10Narratives <vanyatertyshnikov@yandex.ru>
2025-11-30 22:42:41 +0300

    add dart-gateway startup

---
+++ dart-gateway/internal/app/dart-gateway/app.go
@@ -0,0 +1,30 @@
+package gatewayapp
+
+import (
+	"context"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+type App struct {
+	cfg *Config
+	log *zap.Logger
+}
+
+func NewApp(cfg *Config, log *zap.Logger) (*App, error) {
+	return &App{
+		cfg: cfg,
+		log: log,
+	}, nil
+}
+
+func (a *App) Startup(ctx context.Context) error {
+	a.log.Debug("working...")
+	time.Sleep(time.Minute)
+	return nil
+}
+
+func (a *App) Shutdown(ctx context.Context) error {
+	return nil
+}
---
+++ dart-gateway/internal/app/dart-gateway/config.go
@@ -0,0 +1,10 @@
+package gatewayapp
+
+import (
+	"time"
+)
+
+type Config struct {
+	Environment     string        `yaml:"environment" env-default:"dev"`
+	ShutdownTimeout time.Duration `yaml:"shutdown_timeout" env-default:"5s"`
+}
